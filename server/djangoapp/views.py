71dc6ad890bc35406d5873ca6cf6628e7b063d4d
adityakahb <aditya.bhakay@live.com>
2022-11-18 20:54:02 +0530

    Updated Registration page elements

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -28,16 +28,35 @@ def contact(request):
     return render(request, 'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+
+
+def login_request(request):
+    context = {}
+    defaultUrl = 'djangoapp/index.html'
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['userpassword']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            return render(request, defaultUrl, context)
+    else:
+        return render(request, defaultUrl, context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+
+
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
+
+
+def registration_request(request):
+    return render(request, 'djangoapp/registration.html')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 
