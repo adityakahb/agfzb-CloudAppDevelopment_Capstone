61622ea2468258bc1d2426f35c86ae40f6b8789f
adityakahb <aditya.bhakay@live.com>
2022-11-20 10:19:35 +0530

    Package updates and formatting for the js files

--- functions/sample/nodejs/.eslintrc.js
+++ functions/sample/nodejs/.eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true,
-        "node": true
-    },
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
-        "no-multiple-empty-lines": ["error", { "max": 2, "maxBOF": 1 }],
-    }
-}
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'no-multiple-empty-lines': ['error', { max: 2, maxBOF: 1 }]
+  }
+};
