ef44f76258610bcbac1d27df964b7d63a2e2bddc
adityakahb <aditya.bhakay@live.com>
2022-11-21 14:42:16 +0530

    Added models for car model and make

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,6 @@
+import datetime
 from django.db import models
 from django.utils.timezone import now
-
 
 # Create your models here.
 
@@ -10,6 +10,15 @@ from django.utils.timezone import now
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
 
+# Car Make model
+
+
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=48)
+    description = models.CharField(null=True, max_length=480)
+
+    def __str__(self):
+        return self.name
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +28,37 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+
+# Car Model model
+
+
+class CarModel(models.Model):
+    model_make = models.ForeignKey(
+        CarMake, null=True, on_delete=models.CASCADE)
+    model_name = models.CharField(null=False, max_length=50)
+    dealer_id = models.IntegerField(null=True)
+
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    COUPE = "Coupe"
+    WAGON = "Wagon"
+    PICKUP = "Pickup"
+    MINIVAN = "Minivan"
+
+    CAR_CHOICES = [(SEDAN, "Sedan"), (SUV, "SUV"), (COUPE, "Coupe"), (MINIVAN, "Minivan"),
+                   (WAGON, "Wagon"), (PICKUP, "Pick-up truck")]
+    model_type = models.CharField(
+        null=False, max_length=15, choices=CAR_CHOICES, default=SEDAN)
+
+    YEAR_CHOICES = []
+    for r in range(1969, (datetime.datetime.now().year+1)):
+        YEAR_CHOICES.append((r, r))
+
+    model_year = models.IntegerField(
+        ('year'), choices=YEAR_CHOICES, default=datetime.datetime.now().year)
+
+    def __str__(self):
+        return self.model_name + ", " + str(self.model_year) + ", " + self.model_type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
